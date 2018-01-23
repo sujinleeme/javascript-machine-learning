7a6c9682cf53ea6cc7d980c095467e87b9575b81
sujinleeme <sujinlee.me@gmail.com>
2018-01-23 13:30:09 +0900

    Create 404 page & index to let github server can parsing react-router url

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
+import {BrowserRouter} from "react-router-dom"
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(
+	<BrowserRouter basename="/javascript-machine-learning">
+		<App/>
+	
+	</BrowserRouter>
+	, document.getElementById("root")
+)
 registerServiceWorker()
 
 if (module.hot) {
